466fc28655f518bab64e892a878ecd9e1bed1b4b
xifei wu <xfwu@conviva.com>
2023-03-24 14:10:32 +0800

    support resolveJsonModule for ts file

--- http.ts
+++ http.ts
@@ -80,36 +80,6 @@ export async function getFileContentInFormOfStream(targetFile: string) {
     return fs.createReadStream(targetFile);
   }
 }
-var COMPRESSIBLE_TYPE_REGEXP = /^text\/|\+(?:json|text|xml)$/i;
-var EXTRACT_TYPE_REGEXP = /^\s*([^;\s]*)(?:;|\s|$)/;
-/**
- * Checks if a type is compressible.
- *
- * @param {string} type
- * @return {Boolean} compressible
- * @public
- */
-export function compressible(type?: string): boolean {
-  if (!type || typeof type !== 'string') {
-    return false;
-  }
-
-  // strip parameters
-  var match = EXTRACT_TYPE_REGEXP.exec(type);
-  var mime = match && match[1].toLowerCase();
-  if (!mime) {
-    return false;
-  }
-  var data = db[mime];
-
-  // return database information
-  if (data && data.compressible !== undefined) {
-    return data.compressible;
-  }
-
-  // fallback to regexp or unknown
-  return COMPRESSIBLE_TYPE_REGEXP.test(mime);
-}
 
 // export function defaultResponse(response: http.ServerResponse) {
 //   response.writeHead(200, {
