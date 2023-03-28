08030e8da9796d06ab95fcc1b7807b9ac483489d
xifei wu <xfwu@conviva.com>
2023-03-28 17:27:38 +0800

    add function prettyLog

--- log.ts
+++ log.ts
@@ -1,46 +1,63 @@
 import {isObject, formatDate} from './fe';
 import util from 'util';
 
-function log2(content: any) {
-  if (isObject(content)) {
+// function log2(content: any) {
+//   if (isObject(content)) {
+//     console.log(`[${formatDate(Date.now(), 'hh:mm:ss.SSS')}]`);
+//     console.log(
+//       util.inspect(content, {
+//         depth: null,
+//         colors: true,
+//       })
+//     );
+//   } else {
+//     if (Array.isArray(content)) {
+//       content = content.join(', ');
+//     }
+//     console.log(`[${formatDate(Date.now(), 'hh:mm:ss.SSS')}] ${content}`);
+//   }
+// }
+// export function log(content: any): void;
+// export function log(...args: any[]): void;
+// export function log() {
+//   function logOne(content: any) {
+//     if (isObject(content)) {
+//       console.log(
+//         util.inspect(content, {
+//           showHidden: true,
+//           depth: null,
+//           colors: true,
+//         })
+//       );
+//     } else {
+//       if (Array.isArray(content)) {
+//         content = content.join(', ');
+//       }
+//       console.log(`${content}`);
+//     }
+//   }
+//   const params = [].slice.call(arguments) as any[];
+//   if (params.length > 1) {
+//     console.log(`[${formatDate(Date.now(), 'hh:mm:ss.SSS')}]`);
+//     params.forEach(logOne);
+//   } else if (params.length === 1) {
+//     log2(params[0]);
+//   }
+// }
+
+export function prettyLog(content: any): void;
+export function prettyLog(...args: any[]): void;
+export function prettyLog() {
+  function logOne(content: any) {
     console.log(`[${formatDate(Date.now(), 'hh:mm:ss.SSS')}]`);
     console.log(
       util.inspect(content, {
+        showHidden: true,
         depth: null,
         colors: true,
       })
     );
-  } else {
-    if (Array.isArray(content)) {
-      content = content.join(', ');
-    }
-    console.log(`[${formatDate(Date.now(), 'hh:mm:ss.SSS')}] ${content}`);
-  }
-}
-export function log(content: any): void;
-export function log(...args: any[]): void;
-export function log() {
-  function logOne(content: any) {
-    if (isObject(content)) {
-      console.log(
-        util.inspect(content, {
-          showHidden: true,
-          depth: null,
-          colors: true,
-        })
-      );
-    } else {
-      if (Array.isArray(content)) {
-        content = content.join(', ');
-      }
-      console.log(`${content}`);
-    }
   }
   const params = [].slice.call(arguments) as any[];
-  if (params.length > 1) {
-    console.log(`[${formatDate(Date.now(), 'hh:mm:ss.SSS')}]`);
-    params.forEach(logOne);
-  } else if (params.length === 1) {
-    log2(params[0]);
-  }
+  params.forEach(logOne);
 }
