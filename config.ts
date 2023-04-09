86bea49af791234b24a9e188494b9c05f419997e
xifei wu <xfwu@conviva.com>
2023-04-09 09:41:07 +0800

    add file config.ts for global config

---
+++ config.ts
@@ -0,0 +1,15 @@
+export const ports: {
+  [key: string]: {
+    port: number;
+    description: string;
+  }
+} = {
+  netFeature: {
+    port: 3100,
+    description: 'show feature of net related logic, include http, websocket, socks5, etc'
+  },
+  if4Pulse: {
+    port: 3201,
+    description: 'assist for development of instant filter server'
+  }
+}
