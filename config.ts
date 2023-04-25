07e900f12ac8b5e2c6f6a025ff52c45170caf5ca
xifei wu <xfwu@conviva.com>
2023-04-25 19:11:59 +0800

    more port config

--- config.ts
+++ config.ts
@@ -8,8 +8,16 @@ export const PORT: {
     port: 3100,
     description: 'show feature of net related logic, include http, websocket, socks5, etc'
   },
-  if4Pulse: {
+  assistConviva: {
+    port: 3200,
+    description: 'assist for development of assist server'
+  },
+  proxy2Turtle: {
     port: 3201,
-    description: 'assist for development of instant filter server'
+    description: 'proxy server for pulser-turtle'
+  },
+  proxy2App: {
+    port: 3202,
+    description: 'proxy server for pulser-app'
   }
 }
