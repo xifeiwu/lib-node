26e11bba3b93e24185aa9c7be180c57d24eb704e
wuxifei <xfwu@conviva.com>
2023-07-23 23:19:14 +0800

    add dir crypto

---
+++ crypto/hash.ts
@@ -0,0 +1,12 @@
+import {BinaryToTextEncoding, Hash} from 'crypto';
+import {Readable} from 'stream';
+export async function hashStream(hash: Hash, readable: Readable, encode: BinaryToTextEncoding = 'hex') {
+  return new Promise<string>(res => {
+    readable.on('data', (chunk: Buffer) => {
+      hash.update(chunk);
+    });
+    readable.on('end', () => {
+      res(hash.digest('hex'));
+    });
+  });
+}
