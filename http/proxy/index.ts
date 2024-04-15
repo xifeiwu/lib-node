8caf70d578010a129943b71f100d79d264dfad48
xifei wu <xfwu@conviva.com>
2024-04-15 15:08:14 +0800

    export more

--- http/proxy/index.ts
+++ http/proxy/index.ts
@@ -1,4 +1,4 @@
 export {PATHNAME_PROXY_STATUS, startProxyServer} from './server';
-export {proxyRequest} from './handler';
+export * from './handler';
 export * from './utils';
 export * from './types';
