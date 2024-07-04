6e2291955698efac931ca228d1d48fa1eba99404
xifei wu <xfwu@conviva.com>
2024-07-04 14:33:00 +0800

    do refactor on function parseHttpFirstLine, parseHttpHeaderPart

--- net/http/server.test.ts
+++ net/http/server.test.ts
@@ -1,72 +1,70 @@
+import assert from 'assert';
 import {Readable} from 'stream';
 import {parseHttpFirstLine, parseHttpHeaderPart} from './server';
 import {tcpRequestPropsToBuffer} from './client';
+import {getDataFromReadable} from '../../stream';
+import {TcpRequestProps} from '../../types';
 
+const requestProps: TcpRequestProps = {
+  method: 'post',
+  url: '/api/debug/echo',
+  headers: {
+    from: 'test',
+  },
+  data: {
+    a: 1,
+    b: 2,
+  },
+};
 export async function testParseHttpHeaderPart() {
   const reader = new Readable({
     read() {
-      this.push(
-        tcpRequestPropsToBuffer({
-          method: 'post',
-          url: '/api/debug/echo',
-          headers: {
-            from: 'test',
-          },
-          data: {
-            a: 1,
-            b: 2,
-          },
-        })
-      );
+      this.push(tcpRequestPropsToBuffer(requestProps));
       this.push(null);
     },
   });
   const {requestInfo, dataConsumed} = await parseHttpHeaderPart(reader);
-  console.log(`requestInfo`);
-  console.log(requestInfo);
-  console.log(`dataConsumed.toString()`);
-  console.log(dataConsumed.toString());
-  // reader.push(dataConsumed);
-  reader.on('data', chunk => {
-    console.log(`remaining data`);
-    console.log(chunk.toString());
+  assert.deepEqual(requestInfo, {
+    method: 'POST',
+    url: '/api/debug/echo',
+    httpVersion: 'HTTP/1.1',
+    headers: {from: 'test', 'content-length': '13'},
   });
+  assert.equal(
+    dataConsumed.toString(),
+    ['POST /api/debug/echo HTTP/1.1', 'from: test', 'content-length: 13', '', ''].join('\r\n')
+  );
+  const data = await getDataFromReadable(reader);
+  assert.equal(data.toString(), '{"a":1,"b":2}');
 }
 
 export async function testParseHttpFirstLine() {
   const reader = new Readable({
     read() {
-      this.push(
-        tcpRequestPropsToBuffer({
-          method: 'post',
-          url: '/api/debug/echo',
-          headers: {
-            from: 'test',
-          },
-          data: {
-            a: 1,
-            b: 2,
-          },
-        })
-      );
+      this.push(tcpRequestPropsToBuffer(requestProps));
       this.push(null);
     },
   });
   try {
     const {firstLineInfo, dataConsumed: dataConsumed4FirstLine} = await parseHttpFirstLine(reader);
     console.log(`firstLineInfo`);
-    console.log(firstLineInfo);
-    console.log(`dataConsumed4FirstLine.toString()`);
-    console.log(dataConsumed4FirstLine.toString());
-    const {requestInfo, dataConsumed} = await parseHttpHeaderPart(reader, firstLineInfo);
-    console.log(`requestInfo`);
-    console.log(requestInfo);
-    console.log(`dataConsumed.toString()`);
-    console.log(dataConsumed.toString());
-    reader.on('data', chunk => {
-      console.log(`remaining data`);
-      console.log(chunk.toString());
+    assert.deepEqual(firstLineInfo, {
+      method: 'POST',
+      url: '/api/debug/echo',
+      httpVersion: 'HTTP/1.1',
     });
+    assert.equal(dataConsumed4FirstLine.toString(), `POST /api/debug/echo HTTP/1.1\r\n`);
+    const {requestInfo, dataConsumed} = await parseHttpHeaderPart(reader, firstLineInfo);
+
+    assert.deepEqual(requestInfo, {
+      method: 'POST',
+      url: '/api/debug/echo',
+      httpVersion: 'HTTP/1.1',
+      headers: {from: 'test', 'content-length': '13'},
+    });
+    assert.equal(dataConsumed.toString(), ['from: test', 'content-length: 13', '', ''].join('\r\n'));
+    const data = await getDataFromReadable(reader);
+    assert.equal(data.toString(), '{"a":1,"b":2}');
   } catch (err) {
     console.log(err);
   }
--- net/http/server.ts
+++ net/http/server.ts
@@ -1,163 +1,80 @@
 import {Readable} from 'stream';
-import {TcpHttpRequestProps, HttpFirstLineProps} from '../../types';
+import {HttpHeaderPartProps, HttpFirstLineProps} from '../../types';
 import {httpFirstLineReg, httpHeaderLineReg} from '../../constants';
-
-export function getMatcher4LineBreak() {
-  const valueR = 0x0d;
-  const valueN = 0x0a;
-  const status = {
-    matchR: false,
-    matchN: false,
-  };
-  return (n: number) => {
-    if (!status.matchR) {
-      if (n === valueR) {
-        status.matchR = true;
-      }
-    } else {
-      if (n === valueN) {
-        return true;
-      } else {
-        status.matchR = false;
-      }
-    }
-    return false;
-  };
-}
+import {Socket} from 'node:dgram';
+import {getOneLineFromReader} from '../../stream';
 
 interface ParseFirstLineResults {
   firstLineInfo?: HttpFirstLineProps;
   dataConsumed: Buffer;
 }
-export async function parseHttpFirstLine(reader: Readable): Promise<ParseFirstLineResults> {
-  let method: TcpHttpRequestProps['method'];
-  let url: TcpHttpRequestProps['url'];
-  let httpVersion: TcpHttpRequestProps['httpVersion'];
-  const maxLength = 1024;
-  let matcher = getMatcher4LineBreak();
-  let resolve: (v: ParseFirstLineResults) => void;
-  let reject: (err: Error) => void;
-
-  const onReadable = () => {
-    let cacheBuffer = Buffer.alloc(0);
-    try {
-      while (reader.readableLength > 0) {
-        const oneByte = reader.read(1);
-        if (oneByte === null) {
-          break;
-        }
-        cacheBuffer = Buffer.concat([cacheBuffer, oneByte]);
-        if (matcher(oneByte[0])) {
-          const line = cacheBuffer.toString('utf-8').trim().replace(/\r\n$/, '');
-          const execResult = httpFirstLineReg.exec(line);
-          if (execResult) {
-            [method, url, httpVersion] = execResult.slice(1);
-          }
-          break;
-        } else if (cacheBuffer.byteLength > maxLength) {
-          break;
-        }
-      }
-      resolve({
-        firstLineInfo: method !== undefined ? {method, url, httpVersion} : undefined,
-        dataConsumed: cacheBuffer,
-      });
-    } catch (err) {
-      reject(err);
-    }
-  };
-  reader.once('readable', onReadable);
-  return new Promise((res, rej) => {
-    resolve = res;
-    reject = rej;
-  });
+export async function parseHttpFirstLine(reader: Readable): Promise<ParseFirstLineResults | null> {
+  const buffer = await getOneLineFromReader(reader);
+  const line = buffer.toString('utf-8').trim().replace(/\r\n$/, '');
+  const execResult = httpFirstLineReg.exec(line);
+  let firstLineInfo: HttpFirstLineProps;
+  if (execResult) {
+    const [method, url, httpVersion] = execResult.slice(1);
+    firstLineInfo = {method, url, httpVersion};
+  }
+  return {firstLineInfo, dataConsumed: buffer};
 }
-interface ParseHttpHeaderResults<T extends Readable> {
-  requestInfo: TcpHttpRequestProps;
-  reader: T;
+
+interface ParseHttpHeaderResults {
+  requestInfo: HttpHeaderPartProps;
   dataConsumed: Buffer;
 }
 export async function parseHttpHeaderPart<T extends Readable>(
   reader: T,
-  initialValue?: Partial<TcpHttpRequestProps>
-): Promise<ParseHttpHeaderResults<T>> {
-  let requestInfo: TcpHttpRequestProps;
-  let resolve: (v: ParseHttpHeaderResults<T>) => void;
-  let reject: (err: Error) => void;
-  let {method, url, httpVersion, headers = {}} = initialValue ?? {};
-  let dataConsumed = Buffer.alloc(0);
-
-  /**
-   * Update http header value
-   * @param chunk one line
-   * @returns continue or not
-   */
-  const updateValue = (chunk: Buffer) => {
-    const line = chunk.toString('utf-8').trim().replace(/\r\n$/, '');
-    let execResult: RegExpExecArray;
-    if (method === undefined) {
-      execResult = httpFirstLineReg.exec(line);
-      if (execResult) {
-        [method, url, httpVersion] = execResult.slice(1);
-        return true;
-      } else {
-        return false;
-      }
-    } else if ((execResult = httpHeaderLineReg.exec(line))) {
-      const [field, value] = execResult.slice(1);
-      if (!Object.prototype.hasOwnProperty.call(headers, field)) {
-        headers[field] = value;
-      } else {
-        if (!Array.isArray(headers[field])) {
-          headers[field] = [headers[field] as string];
-        }
-        (headers[field] as string[]).push(value);
-      }
-      return true;
-    } else if (line === '') {
-      requestInfo = {
-        method,
-        url,
-        httpVersion,
-        headers,
-      };
-      /** Meets end of header part, will not continue parse logic */
-      return false;
+  firstLineInfo?: HttpFirstLineProps
+): Promise<ParseHttpHeaderResults> {
+  let requestInfo: HttpHeaderPartProps;
+  // let resolve: (v: ParseHttpHeaderResults) => void;
+  // let reject: (err: Error) => void;
+  let dataConsumed: Buffer = Buffer.alloc(0);
+  if (!firstLineInfo) {
+    const parseResult = await parseHttpFirstLine(reader);
+    if (!parseResult.firstLineInfo) {
+      throw new Error(`Parse http first line fail`);
     }
-    return false;
-  };
-
-  const onReadable = () => {
-    let cacheBuffer = Buffer.alloc(0);
-    let matcher = getMatcher4LineBreak();
-    try {
-      while (reader.readableLength > 0) {
-        const oneByte = reader.read(1);
-        if (oneByte === null) {
-          break;
-        }
-        cacheBuffer = Buffer.concat([cacheBuffer, oneByte]);
-        dataConsumed = Buffer.concat([dataConsumed, oneByte]);
-        if (matcher(oneByte[0])) {
-          if (!updateValue(cacheBuffer)) {
-            break;
-          }
-          cacheBuffer = Buffer.alloc(0);
-          matcher = getMatcher4LineBreak();
-        }
-      }
-      resolve({
-        requestInfo,
-        reader,
-        dataConsumed,
-      });
-    } catch (err) {
-      reject(err);
+    requestInfo = {...parseResult.firstLineInfo};
+    dataConsumed = Buffer.concat([dataConsumed, parseResult.dataConsumed]);
+  } else {
+    requestInfo = {...firstLineInfo};
+  }
+  let lineBuffer: Buffer;
+  while ((lineBuffer = await getOneLineFromReader(reader))) {
+    dataConsumed = Buffer.concat([dataConsumed, lineBuffer]);
+    const line = lineBuffer.toString('utf-8').trim().replace(/\r\n$/, '');
+    if (line === '') {
+      break;
     }
-  };
-  reader.once('readable', onReadable);
-  return new Promise<ParseHttpHeaderResults<T>>((res, rej) => {
-    resolve = res;
-    reject = rej;
-  });
+    const execResult = httpHeaderLineReg.exec(line);
+    if (!execResult) {
+      throw new Error(`Format error for http header: ${line}`);
+    }
+    if (!requestInfo['headers']) {
+      requestInfo['headers'] = {};
+    }
+    const {headers} = requestInfo;
+    const [field, value] = execResult.slice(1);
+    if (!Object.prototype.hasOwnProperty.call(headers, field)) {
+      headers[field] = value;
+    } else {
+      if (!Array.isArray(headers[field])) {
+        headers[field] = [headers[field] as string];
+      }
+      (headers[field] as string[]).push(value);
+    }
+  }
+  return {requestInfo, dataConsumed};
+}
+
+export class HttpIncomingMessage extends Readable {
+  socket: Socket;
+  constructor(socket: Socket) {
+    super();
+    this.socket = socket;
+  }
+  parseHeaderPart(socket) {}
 }
