36e4c6bdd789741ae4447e768cd774519a4e2193
xifei wu <xfwu@conviva.com>
2024-08-09 13:14:52 +0800

    move function calDuration from debug.ts to general.ts

--- debug.ts
+++
@@ -1,7 +0,0 @@
-export async function calDuration<T>(promise: Promise<T>) {
-  const start = Date.now();
-  const res = await promise;
-  const end = Date.now();
-  console.log(`time used ${end - start}`);
-  return res;
-}
--- general.ts
+++ general.ts
@@ -130,3 +130,11 @@ export function getBufferMatcher(target: CanConvertToBuffer) {
     return matched;
   };
 }
+
+export async function calDuration<T>(promise: Promise<T>) {
+  const start = Date.now();
+  const res = await promise;
+  const end = Date.now();
+  console.log(`time used ${end - start}`);
+  return res;
+}
