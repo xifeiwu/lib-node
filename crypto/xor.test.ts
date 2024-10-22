5fb9e5d2ea4a037ebc790ce1a5038ba5c1fec805
xifei wu <xfwu@conviva.com>
2024-10-22 14:48:44 +0800

    use case of xor for data transform

--- crypto/xor.test.ts
+++ crypto/xor.test.ts
@@ -1,5 +1,5 @@
-import {Duplex, Transform, pipeline} from 'stream';
-import {getIv, defaultIvBytes, xorData, getXorTransform} from './xor';
+import {Duplex, pipeline} from 'stream';
+import {getIv, xorData, getXorTransform} from './xor';
 import {logColorful, toBuffer} from '..';
 
 export async function testXor() {
@@ -11,17 +11,15 @@ export async function testXor() {
   logColorful({}, enctypted);
 }
 
-export async function asStream() {
+function getClientAndServer() {
   const iv = getIv(3);
   const client = new Duplex({
-    read() {
-      // this.push(xorData('data from client', iv));
-      this.push('data from client');
-    },
+    read() {},
     write(chunk) {
       logColorful({}, 'client consume data:', chunk);
     },
   });
+  client.push('data from client');
   const server = new Duplex({
     read() {},
     write(chunk) {
@@ -31,10 +29,57 @@ export async function asStream() {
       server.push(xorData(response, iv));
     },
   });
+  return {client, server, iv};
+}
+export function pipeDirectly() {
+  const {client, server, iv} = getClientAndServer();
+  pipeline(client, server, err => {
+    console.log(err);
+  });
+  pipeline(server, client, err => {
+    console.log(err);
+  });
+}
+
+export async function xorBetweenStream() {
+  const {client, server, iv} = getClientAndServer();
   pipeline(client, getXorTransform(iv), server, err => {
     console.log(err);
   });
   pipeline(server, getXorTransform(iv), client, err => {
+    console.log(err);
+  });
+}
+
+export async function wrapClientByTwoTransform() {
+  const {client, server, iv} = getClientAndServer();
+  const clientReader = getXorTransform(iv);
+  client.pipe(clientReader);
+  const clientWriter = getXorTransform(iv);
+  clientWriter.pipe(client);
+  pipeline(clientReader, server, err => {
+    console.log(err);
+  });
+  pipeline(server, clientWriter, err => {
+    console.log(err);
+  });
+}
+
+export async function wrapClientByOneDuplex() {
+  const {client, server, iv} = getClientAndServer();
+  const clientWrapper = new Duplex({
+    read() {},
+    write(chunk) {
+      client.write(xorData(chunk, iv));
+    },
+  });
+  client.on('data', chunk => {
+    clientWrapper.push(xorData(chunk, iv));
+  });
+  pipeline(clientWrapper, server, err => {
+    console.log(err);
+  });
+  pipeline(server, clientWrapper, err => {
     console.log(err);
   });
 }
