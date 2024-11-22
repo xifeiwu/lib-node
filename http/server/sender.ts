3eb2be5368d293a6d0a02957e4b896d9f2395a5a
xifei wu <xfwu@conviva.com>
2024-11-22 18:15:01 +0800

    add function sendHttpResponse

---
+++ http/server/sender.ts
@@ -0,0 +1,41 @@
+import http from 'http';
+import {ConnectionPayload, HttpResponseInfo} from '../../types';
+import {updateHeadersByHttpInfo} from '../service/internal';
+import {Readable} from 'stream';
+
+export function sendHttpResponse<Payload extends ConnectionPayload = any>(
+  response: http.ServerResponse,
+  responseInfo: Partial<HttpResponseInfo<Payload>>
+) {
+  const {statusCode, statusMessage, headers = {}, data} = responseInfo;
+  const {
+    dataIsUndefined,
+    dataIsReadable,
+    headers: finalHeaders,
+    data: finalData,
+  } = updateHeadersByHttpInfo({headers, data});
+  for (const [key, value] of Object.entries({statusCode, statusMessage})) {
+    if (value !== undefined) {
+      response[key] = value;
+    }
+  }
+  for (const [name, value] of Object.entries(finalHeaders)) {
+    if (Array.isArray(value)) {
+      for (const item of value) {
+        response.setHeader(name, item);
+      }
+    } else {
+      response.setHeader(name, value);
+    }
+  }
+  if (dataIsUndefined) {
+    response.end();
+  } else {
+    if (dataIsReadable) {
+      (finalData as Readable).pipe(response);
+    } else {
+      response.write(finalData);
+    }
+  }
+  return response;
+}
---
+++ http/service/internal.ts
@@ -0,0 +1,36 @@
+import {isReadable, Readable} from 'stream';
+import querystring, {ParsedUrlQueryInput} from 'querystring';
+import {ConnectionPayload, HttpCommonInfo} from '../../types';
+import {isObject} from '../../external';
+import {convertToBuffer} from '../../transform';
+import {convertKeyToLowerCase} from './common';
+
+export function updateHeadersByHttpInfo(info: HttpCommonInfo) {
+  const {headers: _headers, data} = info;
+  const dataIsUndefined = data === undefined;
+  let dataIsReadable = false;
+  if (dataIsUndefined) {
+    return {...info, dataIsUndefined, dataIsReadable};
+  }
+
+  const headers = convertKeyToLowerCase(_headers);
+  let finalData: ConnectionPayload = data;
+  dataIsReadable = isReadable(finalData as Readable);
+  if (!dataIsUndefined && !dataIsReadable) {
+    const contentType = headers['content-type'];
+    if (typeof contentType === 'string' && contentType.includes('x-www-form-urlencoded') && isObject(data)) {
+      finalData = querystring.stringify(finalData as ParsedUrlQueryInput);
+    }
+    finalData = convertToBuffer(finalData);
+    /** As we try to avoid close connection on client side, so must append content-length on headers */
+    if (!headers['content-length']) {
+      headers['content-length'] = (finalData as Buffer).byteLength;
+    }
+  }
+  return {
+    dataIsUndefined,
+    dataIsReadable,
+    headers,
+    data: finalData,
+  };
+}
