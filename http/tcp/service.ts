769cb45dfc9dd4d0dfcbcc9a10b1d48318063909
xifei wu <xfwu@conviva.com>
2024-11-22 15:02:37 +0800

    deprecate tcpResponsePropsToBuffer

--- http/tcp/service.ts
+++ http/tcp/service.ts
@@ -3,46 +3,17 @@ import {toBuffer, convertToBuffer} from '../../transform';
 import {CanConvertToBuffer, HttpResponseInfo, HttpRequestInfo} from '../../types';
 import {convertKeyToLowerCase, getContentTypeByData} from '../service';
 
-export function tcpResponsePropsToBuffer(info: HttpResponseInfo): Buffer {
-  let {httpVersion, statusCode, statusMessage, headers, data} = info;
-  let bufferArray: CanConvertToBuffer[] = [];
-  if (!/^http\//i.test(httpVersion)) {
-    httpVersion = 'HTTP/' + httpVersion;
-  }
-  const firstLine =
-    [httpVersion, statusCode, statusMessage].map(it => String(it).toUpperCase()).join(' ') + '\r\n';
-  if (data) {
-    const dataBuffer = toBuffer(data);
-    headers['content-length'] = dataBuffer.byteLength + '';
-    headers['connection'] = 'close';
-    bufferArray.push(dataBuffer);
-  }
-
-  const headersLine = Object.entries(headers)
-    .map(([key, value]) => {
-      return `${key}: ${value}` + '\r\n';
-    })
-    .join('');
-  const headerStr = firstLine + headersLine + '\r\n';
-  bufferArray.unshift(headerStr);
-  return toBuffer(bufferArray);
-}
+/**
+ * @deprecated by httpResponseInfoToBuffer
+ * @param info
+ */
+export function tcpResponsePropsToBuffer(info: HttpResponseInfo) {}
 
 function httpInfoToBuffer(
   firstLine: string,
   restInfo?: {headers?: IncomingHttpHeaders | OutgoingHttpHeaders; data?: CanConvertToBuffer}
 ) {
-  const {headers: _headers, data} = restInfo;
-  const headers = convertKeyToLowerCase(_headers);
-  if (!headers['content-type'] && data) {
-    headers['content-type'] = getContentTypeByData(data);
-  }
-  const bufferOfData = convertToBuffer(data);
-  if (bufferOfData.byteLength > 0) {
-    headers['content-length'] = bufferOfData.byteLength + '';
-  } else {
-    headers['content-length'] = 0 + '';
-  }
+  const {headers, data} = restInfo;
   const headerLines = Object.entries(headers)
     .map(([key, value]) => {
       return key + ': ' + value + '\r\n';
@@ -70,3 +41,14 @@ export function httpRequestInfoToBuffer(requestInfo: Partial<HttpRequestInfo>) {
   const firstLine = [method, url, finalHttpVersion].join(' ').toUpperCase();
   return httpInfoToBuffer(firstLine, {headers, data});
 }
+
+// const headers = convertKeyToLowerCase(_headers);
+// if (!headers['content-type'] && data) {
+//   headers['content-type'] = getContentTypeByData(data);
+// }
+// const bufferOfData = convertToBuffer(data);
+// if (bufferOfData.byteLength > 0) {
+//   headers['content-length'] = bufferOfData.byteLength + '';
+// } else {
+//   headers['content-length'] = 0 + '';
+// }
