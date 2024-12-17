ff253636ee3bc74656f8288066353dfc014ed932
xifei wu <xfwu@conviva.com>
2024-12-17 15:13:17 +0800

    add function parseHttpData

--- lib/parse-body/index.ts
+++ lib/parse-body/index.ts
@@ -1,25 +1,25 @@
 import fs from 'fs';
 import path from 'path';
-import {Transform} from 'stream';
+import {isReadable, Readable, Transform} from 'stream';
 import {pipeline} from 'stream/promises';
-import {IncomingMessage} from 'http';
 import {ParserOptions} from './service/types';
 import {getJsonParser, getMultpartParser, getOctetParser} from './parser';
 import {defaultParseOptions, getCacheWriter} from './service/utils';
-import {getIncomingMessageData} from './service/external';
+import {CanConvertToBuffer, getIncomingMessageData, ReadableWithMeta, toReadable} from './service/external';
 
 /**
+ * @deprecated by parseHttpBody
  * Parse http body by params provided on http header part
  * @param request
- * @param parserOptions
+ * @param options
  * @returns undefined/null when there is no data part
  */
-export async function parseBody(request: IncomingMessage, parserOptions?: ParserOptions) {
-  const mregedParserOptions: Required<ParserOptions> = {
+export async function parseBody<DataType = any>(request: ReadableWithMeta, options?: ParserOptions) {
+  const mregedOptions: Required<ParserOptions> = {
     ...defaultParseOptions,
-    ...(parserOptions ?? {}),
+    ...(options ?? {}),
   };
-  const {uploadDir} = mregedParserOptions;
+  const {uploadDir} = mregedOptions;
   /**
    * uploadDir is optional
    * but if uploadDir is set, it must exist or it's parent dir must exist, or will thown error
@@ -38,7 +38,7 @@ export async function parseBody(request: IncomingMessage, parserOptions?: Parser
   const {headers: reqHeaders} = request;
   let parserTransforms: Transform[];
   for (const getParser of [getJsonParser, getOctetParser, getMultpartParser]) {
-    const result = getParser(reqHeaders, mregedParserOptions);
+    const result = getParser(reqHeaders, mregedOptions);
     if (result) {
       parserTransforms = result;
       break;
@@ -51,15 +51,46 @@ export async function parseBody(request: IncomingMessage, parserOptions?: Parser
      */
     // throw new Error(`Parser is not found for content-type: ${reqHeaders['content-type']}`);
     const buffer = await getIncomingMessageData(request);
+    const {'content-type': contentType} = reqHeaders;
+
     if (buffer.byteLength > 0) {
-      return buffer;
+      if (contentType.startsWith('text')) {
+        return buffer.toString() as DataType;
+      }
+      return buffer as DataType;
     }
     return;
   }
-  const {writer, waitCacheData} = getCacheWriter(mregedParserOptions);
+  const {writer, waitCacheData} = getCacheWriter(mregedOptions);
   await pipeline([request, ...parserTransforms, writer]);
   const cacheData = await waitCacheData;
-  return cacheData;
+  return cacheData as DataType;
+}
+
+export async function parseHttpBody<DataType = any>(request: ReadableWithMeta, options?: ParserOptions) {
+  return await parseBody<DataType>(request, options);
+}
+/**
+ * Parse any existing data
+ * @param incomingMessage
+ * @param options
+ * @returns
+ */
+export async function parseHttpData<DataType = any>(
+  incomingMessage: {meta: ReadableWithMeta['headers']; data: CanConvertToBuffer},
+  options?: ParserOptions
+) {
+  const {meta, data} = incomingMessage;
+  let reader: Readable;
+
+  if (isReadable(data as Readable)) {
+    reader = data as Readable;
+  } else {
+    reader = toReadable(data);
+  }
+  // @ts-ignore
+  reader.headers = meta;
+  return await parseHttpBody<DataType>(reader as ReadableWithMeta, options);
 }
 
 export * from './service/types';
--- lib/parse-body/service/external.ts
+++ lib/parse-body/service/external.ts
@@ -1,2 +1,4 @@
+export {toReadable} from '../../../stream';
 export {toBuffer} from '../../../transform';
-export {getIncomingMessageData} from '../../../http';
+export {getIncomingMessageData, parseContentType} from '../../../http';
+export {ReadableWithMeta, CanConvertToBuffer} from '../../../types';
