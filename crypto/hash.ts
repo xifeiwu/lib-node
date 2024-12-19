2b53b080ba7d275c362117b2d3c09e2edcb7350c
xifei wu <xfwu@conviva.com>
2024-12-19 18:05:04 +0800

    add comment to summary way of get hash

--- crypto/hash.ts
+++ crypto/hash.ts
@@ -6,13 +6,16 @@ import {convertToBuffer} from '../transform';
 
 type hashAlgorithm = 'sha1' | 'md5' | 'sha256';
 
+/**
+ * @deprecated by hashData
+ */
 export async function hashStream(
   data: Readable | CanConvertToBuffer,
   config: {algorithm: hashAlgorithm; encode?: BinaryToTextEncoding}
 ) {
   const {algorithm, encode = 'hex'} = config;
   const hash = createHash(algorithm);
-  const readable: Readable = isReadable(data as Readable) ? (data as Readable) : toReadable(data);
+  const readable: Readable = toReadable(data);
   return new Promise<string>(res => {
     readable.on('data', (chunk: Buffer) => {
       hash.update(chunk);
@@ -23,7 +26,20 @@ export async function hashStream(
   });
 }
 
-export function getHashDigest(
+/**
+ * Get hash digest in async way
+ */
+export async function hashData(
+  data: CanConvertToBuffer | Readable,
+  config: {algorithm: hashAlgorithm; encode?: BinaryToTextEncoding}
+) {
+  return await hashStream(data, config);
+}
+
+/**
+ * Get hash digest in sync way
+ */
+export function getHash(
   data: CanConvertToBuffer,
   config: {algorithm: hashAlgorithm; encode?: BinaryToTextEncoding}
 ) {
