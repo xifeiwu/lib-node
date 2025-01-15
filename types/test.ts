29f82070f3971b8ee78873be3919adaea3ecab38
xifei wu <xfwu@conviva.com>
2025-01-15 14:12:56 +0800

    more options for function runFuncTestCases

--- types/test.ts
+++ types/test.ts
@@ -3,20 +3,27 @@
 //   expected: ReturnType;
 //   description?: string[];
 // }
+export type ExpectedAsFunc<FuncType extends (...param: any) => any> = (
+  result: ReturnType<FuncType>
+) => boolean;
 
-export type ExpectedAsFunc<FuncType extends (...param: any) => any> = (result: ReturnType<FuncType>) => boolean;
-export interface FuncTestCase<FuncType extends (...param: any) => any> {
+export interface RunFuncOptions {
+  /** Just run function without result comparison */
+  dryRun?: boolean;
+  /**
+   * If just want to run one test case
+   * Just set global ignore to true, and ignore of the case you want to run to false
+   */
+  ignore?: boolean;
+}
+
+export interface FuncTestCase<FuncType extends (...param: any) => any> extends RunFuncOptions {
   params: Parameters<FuncType>;
   expected: ReturnType<FuncType> | ExpectedAsFunc<FuncType>;
   description?: string[];
-  /** Just run function without result comparison */
-  dryRun?: boolean;
 }
 
-export interface InstanceTestCase<
-  Cls extends  {[key: string]: Function | any},
-  Key extends keyof Cls = any
-> {
+export interface InstanceTestCase<Cls extends {[key: string]: Function | any}, Key extends keyof Cls = any> {
   instance: Cls;
   params: Parameters<Cls[Key]>;
   expected: ReturnType<Cls[Key]>;
