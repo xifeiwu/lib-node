2a25ceda7c8a5663c5f7ac6d2cfdef1e97fa4eaf
xifei wu <xfwu@conviva.com>
2025-02-14 16:49:57 +0800

    add function startTlsClient

--- net/service/client.ts
+++ net/service/client.ts
@@ -1,4 +1,5 @@
 import net, {NetConnectOpts, ServerOpts, Socket, TcpNetConnectOpts} from 'net';
+import tls from 'tls';
 import {GetSocketOptions, HttpUpgradeConfig} from '../../types';
 import {requestAndGetUpgradeInfo} from '../../http';
 
@@ -11,7 +12,31 @@ export function startSocketClient(
 export function startSocketClient(path: string, connectionListener?: () => void): Promise<Socket>;
 export async function startSocketClient(...args) {
   return new Promise<Socket>((res, rej) => {
-    const client = net.createConnection(...(args as [number, string]));
+    const client = net.connect(...(args as [number, string]));
+    client.on('ready', () => {
+      res(client);
+    });
+    client.on('error', err => {
+      rej(err);
+    });
+    client.on('timeout', () => {
+      rej('timeout');
+    });
+  });
+}
+export function startTlsClient(
+  options: NetConnectOpts,
+  connectionListener?: () => void
+): Promise<tls.TLSSocket>;
+export function startTlsClient(
+  port: number,
+  host?: string,
+  connectionListener?: () => void
+): Promise<tls.TLSSocket>;
+export function startTlsClient(path: string, connectionListener?: () => void): Promise<tls.TLSSocket>;
+export async function startTlsClient(...args) {
+  return new Promise<tls.TLSSocket>((res, rej) => {
+    const client = tls.connect(...(args as [number, string]));
     client.on('ready', () => {
       res(client);
     });
