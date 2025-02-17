97d4d8b794418fe538f6255d5cb9bd766d1f3b0b
xifei wu <xfwu@conviva.com>
2025-02-17 14:07:57 +0800

    update annotation

--- child-process/service.ts
+++ child-process/service.ts
@@ -1,9 +1,7 @@
 import fs from 'fs';
-// export const daemonRootDir  = path.join(process.env.HOME, '.daemon');
-// export const socketDir = path.join(daemonRootDir, 'sockets');
 
 /**
- * Spawn js file first if exist, as ts file will cost more resource
+ * Spawn .js file first if exist, as .ts file will cost more resource
  * @param fullPath
  * @returns
  */
