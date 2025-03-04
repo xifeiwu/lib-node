1b56d5f001b98416b960f6f18728a78da6c40eae
xifei wu <xfwu@conviva.com>
2025-03-04 16:00:29 +0800

    add annotation

--- types/stream.ts
+++ types/stream.ts
@@ -18,6 +18,13 @@ export interface SpeedCalOptions {
   maxSampleingCount?: number;
   minIntervalSize?: number;
 }
+
+/**
+ * These values should be set depends on writability of the writer:
+ * 1. if the writability is weak, maxSize should be small to avoid long time waiting
+ * 2. if the writability is fast, it's better not do console action in intervalCb 
+ *    to avoid time consuming that not caused by action of write
+ */
 export interface GetWritabilityOptions extends SpeedCalOptions {
   maxSize?: number | string;
   intervalCb?: (results: WriterSpeedInfo) => void;
