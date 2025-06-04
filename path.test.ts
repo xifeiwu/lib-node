02b8e837220fee338a683ae0e723960c7800c6a6
xifei wu <xfwu@conviva.com>
2025-06-04 17:21:15 +0800

    add logic and test case for getDtFromPath

--- path.test.ts
+++ path.test.ts
@@ -1,68 +1,79 @@
-import {getFilePathInfo} from './path';
+import fs from 'fs';
+import path from 'path';
+import assert from 'assert';
+import {getDtFromPath, getFilePathInfo, getPathWithDtSuffix} from './path';
 import {runFuncTestCases} from './service';
 
 export function testGetFilePathInfo() {
-  runFuncTestCases(
-    getFilePathInfo,
-    [
-      {
-        params: ['/zMovie/modules/lib/node'],
-        expected: {
-          dirname: '/zMovie/modules/lib',
-          basename: 'node',
-          bareBasename: 'node',
-          extname: '',
-        },
+  runFuncTestCases(getFilePathInfo, [
+    {
+      params: ['/zMovie/modules/lib/node'],
+      expected: {
+        dirname: '/zMovie/modules/lib',
+        basename: 'node',
+        bareBasename: 'node',
+        extname: '',
       },
-      {
-        description: ['param is dir'],
-        params: ['/zMovie/modules/lib/node/'],
-        expected: {
-          dirname: '/zMovie/modules/lib',
-          basename: 'node',
-          bareBasename: 'node',
-          extname: '',
-        },
+    },
+    {
+      description: ['param is dir'],
+      params: ['/zMovie/modules/lib/node/'],
+      expected: {
+        dirname: '/zMovie/modules/lib',
+        basename: 'node',
+        bareBasename: 'node',
+        extname: '',
       },
-      {
-        description: ['folder can also be basename'],
-        params: ['zMovie/modules/lib/node/'],
-        expected: {
-          dirname: 'zMovie/modules/lib',
-          basename: 'node',
-          bareBasename: 'node',
-          extname: '',
-        },
+    },
+    {
+      description: ['folder can also be basename'],
+      params: ['zMovie/modules/lib/node/'],
+      expected: {
+        dirname: 'zMovie/modules/lib',
+        basename: 'node',
+        bareBasename: 'node',
+        extname: '',
       },
-      {
-        params: ['zMovie/modules/lib/node/path.test.ts'],
-        expected: {
-          dirname: 'zMovie/modules/lib/node',
-          basename: 'path.test.ts',
-          bareBasename: 'path.test',
-          extname: '.ts',
-        },
+    },
+    {
+      params: ['zMovie/modules/lib/node/path.test.ts'],
+      expected: {
+        dirname: 'zMovie/modules/lib/node',
+        basename: 'path.test.ts',
+        bareBasename: 'path.test',
+        extname: '.ts',
       },
-      {
-        description: ['file path without dirname'],
-        params: ['bar.ts'],
-        expected: {
-          dirname: '.',
-          basename: 'bar.ts',
-          extname: '.ts',
-          bareBasename: 'bar',
-        },
+    },
+    {
+      description: ['file path without dirname'],
+      params: ['bar.ts'],
+      expected: {
+        dirname: '.',
+        basename: 'bar.ts',
+        extname: '.ts',
+        bareBasename: 'bar',
       },
-      {
-        description: ['file path with dirname: ./'],
-        params: ['./bar.ts'],
-        expected: {
-          dirname: '.',
-          basename: 'bar.ts',
-          extname: '.ts',
-          bareBasename: 'bar',
-        },
+    },
+    {
+      description: ['file path with dirname: ./'],
+      params: ['./bar.ts'],
+      expected: {
+        dirname: '.',
+        basename: 'bar.ts',
+        extname: '.ts',
+        bareBasename: 'bar',
       },
-    ],
-  );
+    },
+  ]);
+}
+
+export async function dateTimeSuffix() {
+  const filePath = path.join(__dirname, 'tmp.ts');
+  fs.writeFileSync(filePath, 'aa');
+  assert(fs.existsSync(filePath));
+  const pathWithSuffix = getPathWithDtSuffix(filePath);
+  fs.renameSync(filePath, pathWithSuffix);
+  const dt = getDtFromPath(pathWithSuffix);
+  assert(dt instanceof Date);
+  fs.unlinkSync(pathWithSuffix);
 }
--- path.ts
+++ path.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import {FilePathSegement} from './types';
+import {formatDate} from './external';
 
 export function parseBasename(basename: string) {
   const extname = path.extname(basename);
@@ -21,4 +22,23 @@ export function getFilePathInfo(filePath: string): FilePathSegement {
 export function getPathWithBasenaneSuffix(fullPath: string, suffix: string) {
   const {dirname, bareBasename, extname} = getFilePathInfo(fullPath);
   return path.join(dirname, bareBasename + suffix + extname);
+}
+
+/**
+ * get full path with bareBasename suffixed with date-time string
+ */
+export function getPathWithDtSuffix(fullPath: string) {
+  const suffix = formatDate(new Date(), '-yyyy-MM-ddThh:mm:ss');
+  return getPathWithBasenaneSuffix(fullPath, suffix);
+}
+
+export function getDtFromPath(fullPath: string) {
+  const {bareBasename} = getFilePathInfo(fullPath);
+  const reg = /[\d]{4,4}-[\d]{2,2}-[\d]{2,2}T[\d]{2,2}:[\d]{2,2}:[\d]{2,2}$/;
+  const execResult = reg.exec(bareBasename);
+  if (!execResult) {
+    return null;
+  }
+  const [dateTimeStr] = execResult;
+  return new Date(dateTimeStr);
 }
