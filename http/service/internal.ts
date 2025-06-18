485892aeb63d651387d32d36f76cc9cd2d53fc4c
xifei wu <xfwu@conviva.com>
2025-06-18 15:07:38 +0800

    more thoughts on updateHeadersByHttpInfo

--- http/service/internal.ts
+++ http/service/internal.ts
@@ -1,25 +1,34 @@
 import {isReadable, Readable} from 'stream';
-import {HttpRequestInfo, HttpResponseInfo} from '../../types';
 import {convertKeyToLowerCase, isPlainObject} from '../../external';
 import {inferContentTypeByData} from './common';
 import {OutgoingHttpHeaders} from 'http';
+import {HttpCommonInfo} from '../../types';
 
 /**
- * For consideration of why updateHeaders on httpInfo layer?
- * For logic general and compatibility, this logic is added in lower layer
+ * This function is mainly used to add content-type to headers part if it's value is undefined
  *
+ * For http
  * For key content-length, transfer-encoding of header part, there are some related logic on node http module
  * 1. When request.end() is called, http module knows the byteLength of body, and set it as value of content-length
  * 2. When request.write() called multpile times, `chunked` will be set as value of transfer-encoding
+ *
+ * For tcp
+ * the http action over socket should be wrapped by HttpIncomingMessage or HttpOutgoingMessage,
+ * content-length, transfer-encoding should be set or update before payload sending.
+ * Here is not a good place to set content-length, transfer-encoding, because their values depends on final data.
+ *
  * But there is no special logic for content-type, to avoid set headers.content-type on every httpRequestOptions,
- * if the content-type is not set, it can be referred by function getContentTypeByData
+ * if the content-type is not set, refer it by inferContentTypeByData
  *
  * Change log:
  * 1. Not do data convert
- * 2. Not set content-length, as content-length depends on the final expression of data
+ * 2. Not set content-length, as content-length depends on the final data format
+ *
+ * TODO:
+ * As it's only used for sendHttpRequest, it may move to http/client/send as a internal function
  */
 export function updateHeadersByHttpInfo(
-  info: Partial<HttpRequestInfo> | Partial<HttpResponseInfo>,
+  info: HttpCommonInfo,
   options?: {
     /** set header if not exist */
     supplementHeaders?: OutgoingHttpHeaders;
