c519b8544e5df8803766c75ed9338793cc0cf4e6
xifei wu <xfwu@conviva.com>
2025-07-18 14:55:09 +0800

    add script to run on child process: io-transparent.ts

---
+++ child-process/cp-script/io-transparent.test.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import {spawnScript} from '../spawn';
+import {logColorful} from '../../log';
+
+export async function run() {
+  const {childProcess, spawnConfig, wholeScript} = await spawnScript(
+    path.join(__dirname, 'io-transparent.ts'),
+    {
+      spawnOptions: {
+        stdio: [0, 1, 2],
+      },
+    }
+  );
+  logColorful({}, childProcess.pid, wholeScript);
+}
---
+++ child-process/cp-script/io-transparent.ts
@@ -0,0 +1,14 @@
+import stream from 'stream';
+import {largeDataToString} from '../../transform';
+
+/**
+ * A scipt to test io between parent and child is through
+ */
+const counter = new stream.Transform({
+  transform(chunk, _enc, cb) {
+    process.stdout.write('remote: ' + largeDataToString(chunk, {maxPrintSize: 32}));
+    cb && cb();
+  },
+});
+
+process.stdin.pipe(counter);
