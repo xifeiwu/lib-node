9532f73ab6ef0ff13c5def1863a3369e63b4d3da
xifei wu <xfwu@conviva.com>
2025-07-28 16:12:21 +0800

    add more feature to cp-script/io-transparent.ts

--- child-process/cp-script/io-transparent.ts
+++ child-process/cp-script/io-transparent.ts
@@ -1,13 +1,32 @@
 import stream from 'stream';
 import {largeDataToString} from '../../transform';
+import {coloringContent} from '../../log';
 
+function trimCr(buf: Buffer) {
+  while (buf[buf.byteLength - 1] === 0x0a) {
+    buf = buf.subarray(0, buf.byteLength - 1);
+  }
+  return buf;
+}
+
+type Status = 'normal' | 'exiting';
+let status: Status = 'normal';
 /**
  * A scipt to test whether io connection between parent and child is through
  */
 function start() {
   const counter = new stream.Transform({
-    transform(chunk, _enc, cb) {
-      process.stdout.write('remote: ' + largeDataToString(chunk, {maxPrintSize: 32}));
+    async transform(chunk, _enc, cb) {
+      const str = trimCr(chunk).toString();
+      if (status === 'exiting') {
+        const statusCode = parseInt(str);
+        process.exit(Number.isInteger(statusCode) ? statusCode : -1);
+      } else if (str === 'exit') {
+        status = 'exiting';
+        process.stdout.write(coloringContent({color: 'red'}, 'Please set exit code in number format: '));
+      } else {
+        process.stdout.write('echo: ' + largeDataToString(chunk, {maxPrintSize: 32}));
+      }
       cb && cb();
     },
   });
