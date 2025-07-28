acf078ea18c2a39a15c8d5f17ed045d9235c2a75
xifei wu <xfwu@conviva.com>
2025-07-28 16:13:50 +0800

    add logic syncupDirContentByDiff

--- child-process/exec.ts
+++ child-process/exec.ts
@@ -1,7 +1,9 @@
 import fs from 'fs';
+import path from 'path';
 import {execSync} from 'child_process';
 import {logColorful} from '../log';
 import {convertToBuffer} from '../transform';
+import {goOnOrNot} from '../readline';
 
 /**
  * Whether through error or not depends on the return value of shell command:
@@ -33,13 +35,71 @@ export function diffDir(dir1: string, dir2: string): string {
     throw new Error(`dir not exist: ${dirNotExist}`);
   }
   try {
-    execSync(`diff -uNra ${dir1} ${dir2}`, {encoding: 'utf-8'});
+    logColorful({color: 'yellow'}, `diff -uNra ${dir1} ${dir2}`);
+    execSync(`diff -uNra ${dir1} ${dir2}`, {encoding: 'utf-8', maxBuffer: 1024 * 1024 * 128});
     return '';
-  } catch (e: any) {
-    if (e.status === 1) {
-      return e.stdout.toString();
+  } catch (err: any) {
+    if (err.status === 1) {
+      return err.stdout.toString();
     } else {
-      throw new Error('diff command failed:' + e.stderr?.toString() || e.message);
+      throw new Error('diff command failed:' + err.stderr?.toString() || err.message);
     }
+  }
+}
+
+/**
+ * sync up content of dir ${from} to dir ${to}
+ * diff = to - from
+ * path = diff + from, apply diff to from
+ * @param from, the dir refer to
+ * @param to, the dir to apply patch
+ */
+export async function syncupDirContentByDiff(from: string, to: string) {
+  /** diff folder using absolute path */
+  from = path.resolve(from);
+  to = path.resolve(to);
+  if (!fs.existsSync(from)) {
+    throw new Error(`original folder not found: ${from}`);
+  }
+  if (
+    !fs.existsSync(to) &&
+    (await goOnOrNot({
+      tips: [`Folder not exist, will create it`, to + '?'],
+    }))
+  ) {
+    fs.mkdirSync(to);
+  }
+  const diff = await diffDir(to, from);
+  logColorful({color: 'yellow'}, 'Content of diff:');
+  const toPathParts = to.split('/');
+  const fromPathParts = from.split('/');
+  const index = toPathParts.findIndex((it, index) => {
+    return it !== fromPathParts[index];
+  });
+  const patchDir = toPathParts.slice(0, index + 1).join('/');
+  const dirDepth = index + 1;
+  logColorful({}, diff);
+  if (
+    !(await goOnOrNot({
+      tips: [
+        `This is the diff between`,
+        to,
+        'and',
+        from,
+        `will apply to dir ${patchDir}`,
+        `will you apply these diff?`,
+      ],
+      defaultValue: true,
+    }))
+  ) {
+    return;
+  }
+  try {
+    execSync(`patch -d ${patchDir} -p${dirDepth}`, {input: diff, encoding: 'utf-8'});
+  } catch (error) {
+    const {stdout, stderr, message} = error;
+    const out = stdout?.toString();
+    const err = stderr?.toString();
+    logColorful({}, out, err, message);
   }
 }
