bffa7dce4ea5d81bb9fa34bddec9fe61543f922d
xifei wu <xfwu@conviva.com>
2025-08-01 11:13:03 +0800

    should avoid export some complex and not so common logic from index.ts

--- index.ts
+++ index.ts
@@ -12,13 +12,17 @@ export * from './log';
 export * from './process';
 export * from './child-process';
 export * from './crypto';
-export * from './utils';
 export * from './lib/http-body-parser';
 export * from './lib/socks';
 export * from './lib/cookies';
 export * from './lib/http-proxy';
-export * from './lib/http-record';
 export * from './lib/mime';
 export * from './types';
+/**
+ * To reduce work of tsc compile when using `import * from 'module/lib/node'` from outside,
+ * should avoid export some complex and not so common logic from index.ts
+ */
+// export * from './utils';
+// export * from './lib/http-record';
 // export * from './external';
 // export * from './lib/memcached';
