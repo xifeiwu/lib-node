1f85e271b74ca2f1a81727da8c9f5ed3c04240ba
xifei wu <xfwu@conviva.com>
2025-08-25 18:24:14 +0800

    refactor logic of removeFile

--- fs/other.ts
+++ fs/other.ts
@@ -1,6 +1,89 @@
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * NOTICE:
+ * for link file, fs.existsSync or fs.statSync will throw error even link file exist,
+ * so here use fs.lstatSync to check existence of link file
+ */
+export function isFileExist(filePath: string) {
+  try {
+    const stat = fs.lstatSync(filePath);
+    if (stat) {
+      return true;
+    }
+  } catch {
+    return false;
+  }
+}
+
+/**
+ * Remove file, directory, link
+ * @param filepath
+ * @returns
+ */
+export function removeFile(filepath: string) {
+  const fullpath = path.resolve(filepath);
+  const result: {
+    fullpath: string;
+    stat?: fs.Stats;
+  } = {
+    fullpath,
+  };
+  try {
+    const stat = fs.lstatSync(filepath);
+    result.stat = stat;
+    if (stat.isSymbolicLink() || stat.isFile()) {
+      fs.unlinkSync(filepath);
+    } else if (stat.isDirectory()) {
+      fs.rmdirSync(filepath, {recursive: true});
+    }
+  } catch (err) {
+    return result;
+  }
+}
+
+/**
+ * @deprecated by removeFile
+ * @param path
+ */
+export function recursiveDeleteFile(path: string) {
+  if (fs.existsSync(path)) {
+    if (fs.statSync(path).isFile()) {
+      fs.unlinkSync(path);
+    } else if (fs.statSync(path).isDirectory()) {
+      fs.readdirSync(path).forEach((file, index) => {
+        const curPath = path + '/' + file;
+        if (fs.statSync(curPath).isDirectory()) {
+          // recurse
+          recursiveDeleteFile(curPath);
+        } else {
+          // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(path);
+    }
+  } else {
+    console.error(`Error: path ${path} not exist`);
+  }
+}
+
+export function link(sourceFile: string, targetFile: string) {
+  // link can't be overrided, so remove it first
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error(`binFile not exist: ${sourceFile}`);
+  }
+  targetFile = path.resolve(targetFile);
+  if (isFileExist(targetFile)) {
+    fs.unlinkSync(targetFile);
+  }
+  const relativePath = path.relative(path.dirname(targetFile), sourceFile);
+  fs.symlinkSync(relativePath, targetFile);
+  return {sourceFile, targetFile, relativePath};
+}
+
+
 export function writeFileSync(fullPath: string, data: string | NodeJS.ArrayBufferView) {
   const dirName = path.dirname(fullPath);
   if (!fs.existsSync(dirName)) {
@@ -28,55 +111,4 @@ export function moveFile(fromPath: string, toPath: string) {
     fs.copyFileSync(fromPath, toPath);
     fs.unlinkSync(fromPath);
   }
-}
-
-export function recursiveDeleteFile(path: string) {
-  if (fs.existsSync(path)) {
-    if (fs.statSync(path).isFile()) {
-      fs.unlinkSync(path);
-    } else if (fs.statSync(path).isDirectory()) {
-      fs.readdirSync(path).forEach((file, index) => {
-        const curPath = path + '/' + file;
-        if (fs.statSync(curPath).isDirectory()) {
-          // recurse
-          recursiveDeleteFile(curPath);
-        } else {
-          // delete file
-          fs.unlinkSync(curPath);
-        }
-      });
-      fs.rmdirSync(path);
-    }
-  } else {
-    console.error(`Error: path ${path} not exist`);
-  }
-}
-
-/**
- * NOTICE:
- * for link file, fs.existsSync or fs.statSync will throw error even link file exist,
- * so here use fs.lstatSync to check existence of link file
- */
-function isLinkFileExist(filePath: string) {
-  try {
-    const stat = fs.lstatSync(filePath);
-    if (stat) {
-      return true;
-    }
-  } catch {
-    return false;
-  }
-}
-
-export function link(sourceFile: string, targetFile: string) {
-  // link can't be overrided, so remove it first
-  if (!fs.existsSync(sourceFile)) {
-    throw new Error(`binFile not exist: ${sourceFile}`);
-  }
-  if (isLinkFileExist(targetFile)) {
-    fs.unlinkSync(targetFile);
-  }
-  const relativePath = path.relative(path.dirname(targetFile), sourceFile);
-  fs.symlinkSync(relativePath, targetFile);
-  return {sourceFile, targetFile, relativePath};
 }
