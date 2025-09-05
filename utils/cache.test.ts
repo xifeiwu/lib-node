dc4faec0275d80f3b74317836967b16622251f89
xifei wu <xfwu@conviva.com>
2025-09-05 17:49:44 +0800

    add logic useCachedData in file utils/cache.ts

---
+++ utils/cache.test.ts
@@ -0,0 +1,19 @@
+import assert from 'assert';
+import {logColorful} from '../log';
+import {useCachedData} from './cache';
+
+const maxAge = 3000;
+const {getOrFetch} = useCachedData({maxAge}, async () => {
+  await new Promise(res => setTimeout(res, 1000));
+  return Date.now();
+});
+
+export async function runUseCachedData() {
+  const results = await Promise.all(new Array(10).fill(getOrFetch).map(it => it()));
+  const v1 = await getOrFetch();
+  await new Promise(res => setTimeout(res, maxAge));
+  const v2 = await getOrFetch();
+  logColorful({}, results, v1, v2);
+  assert(results.every(it => it === v1));
+  assert.notEqual(v1, v2);
+}
---
+++ utils/cache.ts
@@ -0,0 +1,67 @@
+import {isFunction} from '../external';
+
+type SyncGetDataSource<T> = T | ((...params: any[]) => T);
+type ASyncGetDataSource<T> = (...params: any[]) => Promise<T>;
+type GetDataSource<T> = SyncGetDataSource<T> | ASyncGetDataSource<T>;
+
+export function useCachedData<T>(options: {maxAge: number}, globalDataSource?: GetDataSource<T>) {
+  const {maxAge} = options;
+  let data: T;
+  let expireAt: number;
+  let promise: Promise<T>;
+  function setSync(dataSource: SyncGetDataSource<T>) {
+    try {
+      if (isFunction(dataSource)) {
+        data = (dataSource as (...params: any[]) => T)();
+      } else {
+        data = dataSource as T;
+      }
+      expireAt = Date.now() + maxAge;
+      return true;
+    } finally {
+      return false;
+    }
+  }
+  async function set(dataSource: GetDataSource<T>) {
+    try {
+      if (promise === undefined) {
+        if (isFunction(dataSource)) {
+          promise = (dataSource as (...params: any[]) => Promise<T>)();
+        } else {
+          promise = Promise.resolve(dataSource as T);
+        }
+      }
+      data = await promise;
+      expireAt = Date.now() + maxAge;
+      return true;
+    } catch (err) {
+      return false;
+    } finally {
+      promise = undefined;
+    }
+  }
+  function get() {
+    if (typeof expireAt === 'number' && Date.now() > expireAt) {
+      return undefined;
+    }
+    return data;
+  }
+  /**
+   * It only works when typeof globalDataSource is SyncGetDataSource<T>
+   * @param dataSource 
+   * @returns 
+   */
+  function getOrFetchSync(dataSource: SyncGetDataSource<T>) {
+    if (get() === undefined) {
+      setSync(dataSource ?? (globalDataSource as SyncGetDataSource<T>));
+    }
+    return data;
+  }
+  async function getOrFetch(dataSource?: GetDataSource<T>) {
+    if (get() === undefined) {
+      await set(dataSource ?? globalDataSource);
+    }
+    return data;
+  }
+  return {setSync, set, get, getOrFetch, getOrFetchSync};
+}
