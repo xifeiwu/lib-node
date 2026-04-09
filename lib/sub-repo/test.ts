3ce396caf4f20f408a02da4398c70fa95ff40426
xifei wu <xfwu@conviva.com>
2026-04-09 14:36:57 +0800

    more test path for sub-repo logic

--- lib/sub-repo/test.ts
+++ lib/sub-repo/test.ts
@@ -2,12 +2,14 @@ import { getSubrepoConfigSummary } from "./service/config";
 import { syncSubreposFromWorkspace } from "./sync";
 
 export async function testGetSubrepoConfigSummary() {
-  const dir = '/Users/wuxifei/code/react/start/small-apps-wrapper';
+  // const dir = '/Users/wuxifei/code/react/start/small-apps-wrapper';
+  const dir = '/Users/wuxifei/code/electron/electron-apps-wrapper';
   const configSummary = getSubrepoConfigSummary(dir);
   console.log(configSummary);
 }
 
 export async function testSyncSubreposFromWorkspace() {
-  const dir = '/Users/wuxifei/code/react/start/small-apps-wrapper';
+  // const dir = '/Users/wuxifei/code/react/start/small-apps-wrapper';
+  const dir = '/Users/wuxifei/code/electron/electron-apps-wrapper';
   await syncSubreposFromWorkspace({dir});
 }
