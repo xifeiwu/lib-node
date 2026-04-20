5f903bda69a837144c06c72d1e41828ecad4313a
xifei wu <xfwu@conviva.com>
2026-04-20 15:41:25 +0800

    add one more test case for monitored mode

--- lib/process-manager/launch-cp/monitored.test.ts
+++ lib/process-manager/launch-cp/monitored.test.ts
@@ -2,11 +2,12 @@ import path from 'path';
 import {logColorful, getSpawnConfigByScript, spwanInDetachedMode, CP} from '../service/external';
 import type {LaunchCpConfig} from '../service';
 import {readProcInfo} from '../service';
+import {launchCpInMonitoredMode} from './monitored';
 
 const debugServerScript = path.resolve(__dirname, '../../../utils/cp-script/debug-server.ts');
 const monitoredScript = path.resolve(__dirname, './monitored.ts');
 
-export async function runMonitoredDebugServer() {
+export async function runMonitoredDebugServerInDetachedMode() {
   const cpId = 'monitored-debug-server';
   const config: LaunchCpConfig = {
     id: cpId,
@@ -32,6 +33,34 @@ export async function runMonitoredDebugServer() {
   });
   const {finalSpawnConfig, ...spawnResponse} = await spwanInDetachedMode(monitorSpawnConfig);
   logColorful({}, 'Monitor process spawned (detached):', spawnResponse);
+
+  const persisted = readProcInfo(cpId);
+  logColorful({}, 'Persisted info:', persisted);
+}
+
+/**
+ * Start monitor and script process not in detached mode
+ * Can be used for debugging
+ */
+export async function runMonitoredDebugServer() {
+  const cpId = 'monitored-debug-server';
+  const info = await launchCpInMonitoredMode({
+    id: cpId,
+    spawnConfig: getSpawnConfigByScript<CP.DebugServerConfig>(debugServerScript, {
+      params: [cpId],
+      infoToCp: {
+        port: 3457,
+      },
+      maxWaitTime4Ipc: 10,
+    }),
+    monitorConfig: {
+      retry: {
+        maxCount: 3,
+        minInterval: 5000,
+      },
+    },
+  });
+  logColorful({}, 'LaunchCp monitored info:', info);
 
   const persisted = readProcInfo(cpId);
   logColorful({}, 'Persisted info:', persisted);
