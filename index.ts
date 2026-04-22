d25105c964d3edaa913180f0fab6be4a665d8c29
xifei wu <xfwu@conviva.com>
2026-04-22 18:37:41 +0800

    don't rexport lib from index.ts

--- index.ts
+++ index.ts
@@ -13,15 +13,10 @@ export * from './process';
 export * from './child-process';
 export * from './crypto';
 export * from './lib/http-body-parser';
-export * from './lib/socks';
-export * from './lib/cookies';
-export * from './lib/http-proxy';
 export * from './types';
 /**
  * To reduce work of tsc compile when using `import * from 'module/lib/node'` from outside,
  * should avoid export some complex and not so common logic from index.ts
  */
 // export * from './utils';
-// export * from './lib/http-record';
 // export * from './external';
-// export * from './lib/memcached';
