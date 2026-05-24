845ec1f3dab3e88167977924147d2e9fefd3563a
xifei wu <xfwu@conviva.com>
2026-05-25 00:42:18 +0800

    remove duplicated test case

--- lib/assets-management/test/operation/3-assets-backup.ts
+++ lib/assets-management/test/operation/3-assets-backup.ts
@@ -1,6 +1,6 @@
 import {updateMetaHandlerMeta} from '../../operation/meta-align';
 import {getFileMetaHandler} from '../../service';
-import {handleAssetsBackup} from '../../operation/assets-backup';
+import {backupAssets} from '../../operation/assets-backup';
 import {initSampleAssets} from '../../file-generator';
 import {SOURCE_DIR, TARGET_DIR} from '../serivice';
 
@@ -14,7 +14,7 @@ export async function testHandleAssetsBackup() {
   await updateMetaHandlerMeta(metaHandlers);
   const bkMetaHandlers = await getFileMetaHandler()(TARGET_DIR);
   await updateMetaHandlerMeta(bkMetaHandlers);
-  await handleAssetsBackup(bkMetaHandlers, metaHandlers);
+  await backupAssets(bkMetaHandlers, await metaHandlers.getMeta());
 }
 export async function testHandleAssetsBackup2() {
   // const rootDir = '/Volumes/ssd_4t/z-movie';
@@ -28,5 +28,5 @@ export async function testHandleAssetsBackup2() {
   const bkMetaHandlers = await getFileMetaHandler()(bkrootDir);
   await bkMetaHandlers.getMeta();
   await updateMetaHandlerMeta(bkMetaHandlers);
-  await handleAssetsBackup(bkMetaHandlers, metaHandlers);
+  await backupAssets(bkMetaHandlers, await metaHandlers.getMeta());
 }
